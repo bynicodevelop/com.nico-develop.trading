83de87d4c5df0b59d25cee2b9b908ee45455fc82
nico-develop <contact@nico-develop.fr>
2023-04-23 12:16:43 +0200

    [TEST] Ajout des tests pour le service account

---
+++ packages/account/src/account.spec.ts
@@ -0,0 +1,47 @@
+import {
+	Account,
+	IConnectorService,
+} from '@packages/common';
+
+import { AccountService } from './account';
+
+describe('AccountService', () => {
+	let accountService: AccountService;
+	let connectorServiceMock: jest.Mocked<IConnectorService>;
+
+	beforeEach(() => {
+		connectorServiceMock = {
+			getAccount: jest.fn(),
+		} as unknown as jest.Mocked<IConnectorService>;
+
+		accountService = new AccountService(connectorServiceMock);
+	});
+
+	describe('getAccount', () => {
+		it('should return an Account object', async () => {
+			const expectedAccount: Account = {
+				id: 'my-account-id',
+				cash: 1000,
+			} as Account;
+
+			connectorServiceMock.getAccount.mockResolvedValueOnce(expectedAccount);
+
+			const account = await accountService.getAccount();
+
+			expect(account).toEqual(expectedAccount);
+			expect(connectorServiceMock.getAccount).toHaveBeenCalledTimes(1);
+		});
+
+		it('should throw an error if there is an error getting the account', async () => {
+			const errorMessage = 'An error occurred while getting the account';
+			connectorServiceMock.getAccount.mockRejectedValueOnce(
+				new Error(errorMessage)
+			);
+
+			await expect(accountService.getAccount()).rejects.toThrowError(
+				errorMessage
+			);
+			expect(connectorServiceMock.getAccount).toHaveBeenCalledTimes(1);
+		});
+	});
+});
